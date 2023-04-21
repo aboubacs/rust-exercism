8be6dd1275072181b3084ffaa529b96d0ff7ae71
Salim Aboubacar <salimaboubacar@gmail.com>
2023-04-21 21:42:19 +0200

    Work with unicode

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,7 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    // input.chars().rev().collect()
+    // work with graphemes:
+    input.graphemes(true).rev().collect()
 }
